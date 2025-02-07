f786f3456d1f1d195170e7bc369306538c544aad
Suraju oladimeji <ayodejioladimeji@gmail.com>
2025-02-07 16:52:54 +0000

    Your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   }
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+module.exports = nextConfig;
